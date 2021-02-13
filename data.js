e041d5e586199fe131e244abfc247c33d81c2921
MugeMeggy <mafromist@gmail.com>
2021-02-13 21:38:24 +0000

    #topTrumpsGame #6
     JSON file is back

--- data.js
+++ data.js
@@ -1,32 +1,11 @@
-/* async function getCard() {
-    const response = await fetch("./database.json");
-    const data = await response.json();
-    console.log(data);
-}
+const fs = require("fs");
+const dataBuffer = fs.readFileSync("database1.json");
+const dataJson = dataBuffer.toString();
+const data = JSON.parse(dataJson)
 
-getCard();
- */
+console.log(data.length)
+let cardNum = Math.floor(Math.random() * (data.length));
+console.log(cardNum)
+console.log(data[cardNum]) //how to reach the data 
+console.log(data[cardNum].name)
 
-/* 
-fetch('./database.json')
-  .then(response => response.json())
-  .then(obj => console.log(obj)) */
-
-
-const myInit = { method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                mode: 'cors',
-                cachhe: 'default' 
-            };
-
-let myRequest = new Request("database.json", myInit);
-
-fetch(myRequest)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data.magic)
-    })
